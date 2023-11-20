b580a11b5248c8031e2f8fc26baebbe8aaa9d378
offleu <leonardo.rlima09@gmail.com>
2023-11-20 00:45:22 -0300

    firabase

---
+++ pacientes.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDABxRv7TeJCvrJqJxpdK-WAvqElPb8Ex0",
+  authDomain: "site-maria-julia.firebaseapp.com",
+  databaseURL: "https://site-maria-julia-default-rtdb.firebaseio.com",
+  projectId: "site-maria-julia",
+  storageBucket: "site-maria-julia.appspot.com",
+  messagingSenderId: "641579312371",
+  appId: "1:641579312371:web:3e7abda6a092df4db58386",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ 
